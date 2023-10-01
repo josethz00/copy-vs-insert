eed6eb6d2796e82b2902f5e1be103211aafc2a81
josethz00 <jose.soares18@fatec.sp.gov.br>
2023-10-01 20:05:33 -0300

    fix: close reject¬

--- main.ts
+++ main.ts
@@ -15,31 +15,47 @@ const numRecords = 100000;
 const data = Array.from({ length: numRecords }, (_, i) => [`data_${i}`, new Date()]);
 
 async function measureInsert() {
-    const insertQuery = `
-        INSERT INTO test_table (data, timestamp) VALUES 
-        ${data.map(() => '(($1, $2))').join(',')}
-    `;
-
+    const chunkSize = 1000;
     const startTime = Date.now();
-    await client.query(insertQuery, data.flat());
+
+    for (let i = 0; i < data.length; i += chunkSize) {
+        const chunk = data.slice(i, i + chunkSize);
+        const placeholders = chunk.map((_, index) => `($${index * 2 + 1}, $${index * 2 + 2})`).join(',');
+        const values = chunk.flat();
+
+        const insertQuery = `
+            INSERT INTO test_table (data, time_added) VALUES ${placeholders}
+        `;
+
+        await client.query(insertQuery, values);
+    }
+
     const duration = (Date.now() - startTime) / 1000;
     console.log(`Multi-line INSERT took: ${duration} seconds`);
 }
 
+
 async function measureCopy() {
+    return new Promise<void>((resolve, reject) => {
     const writableStream = fs.createWriteStream("data.csv");
 
     writableStream.on("finish", async () => {
-        const startTime = Date.now();
-        const stream = fs.createReadStream("data.csv");
-        const copyPsqlStream = client.query(copyFrom('COPY test_table (data, timestamp) FROM STDIN CSV'));
-        await pipeline(stream, copyPsqlStream);
-        const duration = (Date.now() - startTime) / 1000;
-        console.log(`COPY took: ${duration} seconds`);
+        try {
+            const startTime = Date.now();
+            const stream = fs.createReadStream("data.csv");
+            const copyPsqlStream = client.query(copyFrom('COPY test_table (data, time_added) FROM STDIN WITH (FORMAT CSV)'));
+            await pipeline(stream, copyPsqlStream);
+            const duration = (Date.now() - startTime) / 1000;
+            console.log(`COPY took: ${duration} seconds`);
+            resolve();
+        } catch (err) {
+            reject(err);
+        }
     });
 
     data.forEach(d => writableStream.write(d.join(',') + '\n'));
     writableStream.end();
+});
 }
 
 async function main() {
