febd366890ceb83ed7168d89cac14d5eece23ded
josethz00 <jose.soares18@fatec.sp.gov.br>
2023-10-01 19:40:12 -0300

    feat: rmeove id insertion

--- main.ts
+++ main.ts
@@ -12,12 +12,12 @@ const client = new Client({
 });
 
 const numRecords = 100000;
-const data = Array.from({ length: numRecords }, (_, i) => [i, `data_${i}`, new Date()]);
+const data = Array.from({ length: numRecords }, (_, i) => [`data_${i}`, new Date()]);
 
 async function measureInsert() {
     const insertQuery = `
-        INSERT INTO test_table (id, data, timestamp) VALUES 
-        ${data.map(() => '(($1, $2, $3))').join(',')}
+        INSERT INTO test_table (data, timestamp) VALUES 
+        ${data.map(() => '(($1, $2))').join(',')}
     `;
 
     const startTime = Date.now();
@@ -32,7 +32,7 @@ async function measureCopy() {
     writableStream.on("finish", async () => {
         const startTime = Date.now();
         const stream = fs.createReadStream("data.csv");
-        const copyPsqlStream = client.query(copyFrom('COPY test_table FROM STDIN CSV'));
+        const copyPsqlStream = client.query(copyFrom('COPY test_table (data, timestamp) FROM STDIN CSV'));
         await pipeline(stream, copyPsqlStream);
         const duration = (Date.now() - startTime) / 1000;
         console.log(`COPY took: ${duration} seconds`);
